375c77ccffc1732953b5d042e3615eb5499f3e98
Anagas <115894733+anagas-s@users.noreply.github.com>
2024-01-04 21:05:53 +0530

    feat: Implement todo deletion

--- Todo-list/app.js
+++ Todo-list/app.js
@@ -6,6 +6,7 @@ const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
 todoButton.addEventListener('click',addTodo);
+todoList.addEventListener('click',deleteCheck);
 
 
 //Functions
@@ -18,23 +19,42 @@ function addTodo(event){
     todoDiv.classList.add('todo');
     //Create li
     const newTodo = document.createElement('li');
-    newTodo.innerText = "hey";
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
 
     //Check Mark Button
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"></i>'
-    completedButton.classList.add('completed-btn');
+    completedButton.classList.add('complete-btn');
     todoDiv.appendChild(completedButton);
 
      //Trash Button
      const trashButton = document.createElement('button');
      trashButton.innerHTML = '<i class="fas fa-trash"></i>'
-     trashButton.classList.add('completed-btn');
+     trashButton.classList.add('trash-btn');
      todoDiv.appendChild(trashButton);
 
      //Append to List
      todoList.appendChild(todoDiv)
+
+     //Clear Todo Input Value
+     todoInput.value="";
     
 } 
+
+
+function deleteCheck(e){
+    const item = e.target;
+    //Delete TODO
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    if(item.classList[0] === "complete-btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed")
+    }
+
+}
