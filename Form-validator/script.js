69184783ef40b90be0b00559ee6159e81b69437a
Anagas <115894733+anagas-s@users.noreply.github.com>
2024-01-08 23:30:56 +0530

    Implement form validation functionality using javascript

---
+++ Form-validator/script.js
@@ -0,0 +1,45 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
+
+
+//Show error outline
+function showError(input,message){
+    const formControl = input.parentElement;
+    formControl.className = "form-control error";
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+//Show success outline
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+//Check email is valid
+function isValidEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+//check required fields
+function checkRequired(inputArr){
+    inputArr.forEach((item)=>{
+        if(item.value.trim() === ""){
+            showError(item,`${item.id.charAt(0).toUpperCase()+ item.id.slice(1)} is required`)
+        }else{
+            showSuccess(item)
+        }
+    })
+}
+
+
+
+//Event Listeners
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    checkRequired([username,email,password,password2]);
+})
