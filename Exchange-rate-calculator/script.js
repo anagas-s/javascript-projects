a11b34f2da18a053aac4ebdddfa22e42d90ad85f
Anagas <115894733+anagas-s@users.noreply.github.com>
2024-01-20 23:47:00 +0530

    Fetch data from external api

--- Exchange-rate-calculator/script.js
+++ Exchange-rate-calculator/script.js
@@ -7,3 +7,23 @@ const rateEl = document.getElementById('rate')
 const swap = document.getElementById('swap')
 
 
+//Fetch exchange rates and update the DOM
+function calculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+    fetch(`https://v6.exchangerate-api.com/v6/e7808b29f9c653fcac57b115/latest/${currency_one}`)
+        .then(res=>res.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+
+
+
+//Event Listeners
+currencyEl_one.addEventListener('change',calculate)
+amountEl_one.addEventListener('input',calculate)
+currencyEl_two.addEventListener('change',calculate)
+amountEl_two.addEventListener('input',calculate)
+
+calculate();
