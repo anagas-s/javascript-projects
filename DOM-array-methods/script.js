d18fdefbbf822d40080ccc39176852bb652ffef5
Anagas <115894733+anagas-s@users.noreply.github.com>
2024-01-21 18:37:18 +0530

    Calculate total wealth using array reduce method

--- DOM-array-methods/script.js
+++ DOM-array-methods/script.js
@@ -55,6 +55,7 @@ function formatMoney(number){
     return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
 
+//Double the money
 function doubleMoney() {
     data = data.map((user)=> {
         return {...user , money: user.money*2 }
@@ -62,16 +63,26 @@ function doubleMoney() {
     updateDOM();
 }
 
-
+//Sort users by richest
 function sortByRichest(){
     data.sort((a,b) => b.money - a.money);
 
     updateDOM();
 }
 
+//Filter only millionaires
 function showMillionaires(){
     data = data.filter((item)=> item.money >= 1000000);
     updateDOM();
+}
+
+//Calculate total wealth
+function calculateWealth(){
+    const wealth = data.reduce((acc,user) => (acc+user.money) ,0);
+
+    const wealthEl = document.createElement('div')
+    wealthEl.innerHTML =  `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong><h3>`
+    main.appendChild(wealthEl);
 }
 
 
@@ -80,3 +91,4 @@ addUserBtn.addEventListener('click',getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click',sortByRichest);
 showMillionairesBtn.addEventListener('click',showMillionaires);
+calculateWealthBtn.addEventListener('click',calculateWealth);
