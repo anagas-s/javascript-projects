8ca1392d428cdc1d155ad826442574ea01692823
Anagas <115894733+anagas-s@users.noreply.github.com>
2024-01-21 11:57:58 +0530

    Fetch random user from external api using async await

--- DOM-array-methods/script.js
+++ DOM-array-methods/script.js
@@ -4,3 +4,32 @@ const doubleBtn = document.getElementById('double')
 const showMillionairesBtn = document.getElementById('show-millionaires')
 const sortBtn = document.getElementById('sort')
 const calculateWealthBtn = document.getElementById('calculate-wealth')
+
+let data = [];
+
+
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+//fetch random user and add money 
+
+async function getRandomUser() {
+    const data = await fetch("https://randomuser.me/api/");
+    const json = await data.json();
+    
+    const user = json.results[0];
+
+    const newUser = {
+        name :`${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random()*1000000),
+    }
+
+    addData(newUser);
+}
+
+
+//Add new obj to data array
+function addData(obj){
+    data.push(obj);
+}
